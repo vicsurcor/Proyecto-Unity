f6e0e7deb19c3d2462296ba1b973b5c2159d9a40
vicsurcor <vdsurcor@gmail.com>
2023-10-30 19:06:25 +0100

    Salto

--- Assets/Full Assets/Scripts/PlayerController.cs
+++ Assets/Full Assets/Scripts/PlayerController.cs
@@ -29,7 +29,9 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate(){
+
         checkMovement();
+
     }
 
     public void checkMovement()
@@ -38,6 +40,10 @@ public class PlayerController : MonoBehaviour
 
         rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
 
+        if (CheckGround.isGrounded){
+            animator.SetBool("is Jumping", false);
+        }
+
         if (Math.Abs(horizontal) != 0f){
             animator.SetBool("is Running", true);
         }
@@ -45,11 +51,7 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("is Running", false);
         }
 
-        if (animator.GetBool("is Jumping") == true && CheckGround.isGrounded){
-
-            animator.SetBool("is Jumping", false);
-
-        }
+        
 
     
         if (isFacingRight && horizontal < 0f){
@@ -76,18 +78,15 @@ public class PlayerController : MonoBehaviour
 
     public void Jump(){
 
-        if (CheckGround.isGrounded){
+        if (!CheckGround.isGrounded){
 
-            
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-
+        
             animator.SetBool("is Jumping", true);
 
         }
-        
-        
-
-        
-
+        else 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
     }
 }
