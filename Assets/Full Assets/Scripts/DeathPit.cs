6ef727137a26cda8104c0daa5fda9886da764594
vicsurcor <vdsurcor@gmail.com>
2023-11-03 19:43:25 +0100

    InicioScript

---
+++ Assets/Full Assets/Scripts/DeathPit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathPit : MonoBehaviour
+{
+
+    public Scene scene;
+    // Start is called before the first frame update
+    public void OnTriggerEnter2D(Collider2D collider){
+
+        
+
+    }
+}
